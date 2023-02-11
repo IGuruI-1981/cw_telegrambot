4e7df85a80be25f739c5145badebdd75ea4db56f
Viktor <viktor.ihtiyarov@gmail.com>
2023-02-11 18:26:29 +0700

    Commit

---
+++ src/main/java/pro/sky/telegrambot/entity/NotificationTask.java
@@ -0,0 +1,51 @@
+package pro.sky.telegrambot.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class NotificationTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private long userId;
+    private String message;
+    private LocalDateTime notificationDateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return message;
+    }
+
+    public void setText(String text) {
+        this.message = text;
+    }
+
+    public LocalDateTime getNotificationDateTime() {
+        return notificationDateTime;
+    }
+
+    public void setNotificationDateTime(LocalDateTime notificationDateTime) {
+        this.notificationDateTime = notificationDateTime;
+    }
+}
